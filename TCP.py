42a9213a1c696b84e23091ea28194b607f0bd0ed
zhang bowen <2585783520@qq.com>
2024-04-12 07:16:02 +0000

    initialization

---
+++ TCP.py
@@ -0,0 +1,49 @@
+import threading
+import time
+import socket
+# 构建TCP 数据 header
+
+
+class TCPSocket():
+
+    def __init__(self, rate=None, debug=True):
+        super().__init__(rate=rate)
+        self._rate = rate
+        self._send_to = None
+        self._recv_from = None
+        
+    def bind():
+        pass
+
+
+    def listen():
+        pass
+
+
+    def accept(self,  address:(str, int)):
+    
+        pass
+    
+
+
+    def connect():
+        pass
+
+    
+
+
+    def recv():
+        pass
+    
+
+
+    def send():
+        pass
+    
+
+
+    def close():
+        pass
+
+
+
